653b5e49bf9838af6aef1a42fa8b032d4558ad06
Andrew Rublyov <andrewrublyov99@gmail.com>
2021-02-22 03:35:18 +0200

    #8: Finish covering logic after refactoring

--- FileFS.Client.Tests/FileFsClientTests/FileFsClientGenericTests.cs
+++ FileFS.Client.Tests/FileFsClientTests/FileFsClientGenericTests.cs
@@ -5,6 +5,8 @@ using FileFS.Client.Transactions.Abstractions;
 using FileFS.DataAccess.Constants;
 using FileFS.DataAccess.Entities;
 using FileFS.DataAccess.Entities.Enums;
+using FileFS.DataAccess.Exceptions;
+using FileFS.DataAccess.Extensions;
 using FileFS.DataAccess.Repositories.Abstractions;
 using FileFS.Tests.Shared.Comparers;
 using Moq;
@@ -94,6 +96,223 @@ namespace FileFS.Client.Tests.FileFsClientTests
 
             // Assert
             Assert.Throws<InvalidNameException>(Act);
+        }
+
+        [Theory]
+        [InlineData("/from", "/to")]
+        public void Rename_WhenEntryNotExists_ShouldThrowException(string oldFileName, string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(oldFileName))
+                .Returns(false);
+
+            // Act
+            void Act() => _client.Rename(oldFileName, newFileName);
+
+            // Assert
+            Assert.Throws<EntryNotFoundException>(Act);
+        }
+
+        [Theory]
+        [InlineData("/from", "/to")]
+        public void Rename_WhenDestinationEntryExists_ShouldThrowException(string oldFileName, string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(oldFileName))
+                .Returns(true);
+
+            _entryRepositoryMock
+                .Setup(r => r.Exists(newFileName))
+                .Returns(true);
+
+            // Act
+            void Act() => _client.Rename(oldFileName, newFileName);
+
+            // Assert
+            Assert.Throws<EntryAlreadyExistsException>(Act);
+        }
+
+        [Theory]
+        [InlineData("/from/from", "/to/to")]
+        public void Rename_WhenParentsNotMatch_ShouldThrowException(string oldFileName, string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(oldFileName))
+                .Returns(true);
+
+            // Act
+            void Act() => _client.Rename(oldFileName, newFileName);
+
+            // Assert
+            Assert.Throws<ArgumentNonValidException>(Act);
+        }
+
+        [Theory]
+        [InlineData("/to")]
+        public void Rename_WhenEntryIsRootDirectory_ShouldThrowException(string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(PathConstants.RootDirectoryName))
+                .Returns(true);
+
+            // Act
+            void Act() => _client.Rename(PathConstants.RootDirectoryName, newFileName);
+
+            // Assert
+            Assert.Throws<OperationIsInvalid>(Act);
+        }
+
+        [Theory]
+        [InlineData("/from", "/to")]
+        public void Move_WithValidParameters_ShouldCallMove(string oldFileName, string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(oldFileName))
+                .Returns(true);
+
+            // Act
+            _client.Move(oldFileName, newFileName);
+
+            // Assert
+            _entryRepositoryMock.Verify(r => r.Move(oldFileName, newFileName));
+        }
+
+        [Theory]
+        [InlineData("file$name", "/valid")]
+        [InlineData("", "/valid")]
+        [InlineData(null, "/valid")]
+        [InlineData("()9", "/valid")]
+        [InlineData("+++", "/valid")]
+        [InlineData("#sdfd", "/valid")]
+        [InlineData("\\/\\/asdasd", "/valid")]
+        [InlineData("!!!asdasd!!!", "/valid")]
+        [InlineData("[dapk_xantep]", "/valid")]
+        [InlineData("&lol&", "/valid")]
+        [InlineData("%%", "/valid")]
+        [InlineData("/valid", "file$name")]
+        [InlineData("/valid", "")]
+        [InlineData("/valid", null)]
+        [InlineData("/valid", "()9")]
+        [InlineData("/valid", "+++")]
+        [InlineData("/valid", "#sdfd")]
+        [InlineData("/valid", "\\/\\/asdasd")]
+        [InlineData("/valid", "!!!asdasd!!!")]
+        [InlineData("/valid", "[dapk_xantep]")]
+        [InlineData("/valid", "&lol&")]
+        [InlineData("/valid", "%%")]
+        [InlineData("432$$4", "%%")]
+        public void Move_WithOneOfNamesIsInvalid_ShouldThrowException(string oldFileName, string newFileName)
+        {
+            // Act
+            void Act() => _client.Move(oldFileName, newFileName);
+
+            // Assert
+            Assert.Throws<InvalidNameException>(Act);
+        }
+
+        [Theory]
+        [InlineData("/from", "/to")]
+        public void Move_WhenEntryNotExists_ShouldThrowException(string oldFileName, string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(oldFileName))
+                .Returns(false);
+
+            // Act
+            void Act() => _client.Move(oldFileName, newFileName);
+
+            // Assert
+            Assert.Throws<EntryNotFoundException>(Act);
+        }
+
+        [Theory]
+        [InlineData("/from", "/to")]
+        public void Move_WhenDestinationEntryExists_ShouldThrowException(string oldFileName, string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(oldFileName))
+                .Returns(true);
+
+            _entryRepositoryMock
+                .Setup(r => r.Exists(newFileName))
+                .Returns(true);
+
+            // Act
+            void Act() => _client.Move(oldFileName, newFileName);
+
+            // Assert
+            Assert.Throws<EntryAlreadyExistsException>(Act);
+        }
+
+        [Theory]
+        [InlineData("/from", "/to/to")]
+        public void Move_WhenDestinationParentEntryNotExists_ShouldThrowException(string oldFileName, string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(oldFileName))
+                .Returns(true);
+
+            _directoryRepositoryMock
+                .Setup(r => r.Exists(newFileName.GetParentFullName()))
+                .Returns(false);
+
+            _entryRepositoryMock
+                .Setup(r => r.Exists(newFileName))
+                .Returns(false);
+
+            // Act
+            void Act() => _client.Move(oldFileName, newFileName);
+
+            // Assert
+            Assert.Throws<DirectoryNotFoundException>(Act);
+        }
+
+        [Theory]
+        [InlineData("/to")]
+        public void Move_WhenEntryIsRootDirectory_ShouldThrowException(string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(PathConstants.RootDirectoryName))
+                .Returns(true);
+
+            // Act
+            void Act() => _client.Move(PathConstants.RootDirectoryName, newFileName);
+
+            // Assert
+            Assert.Throws<OperationIsInvalid>(Act);
+        }
+
+        [Theory]
+        [InlineData("/from", "/from/to")]
+        public void Move_WhenDestinationIsChildToSource_ShouldThrowException(string oldFileName, string newFileName)
+        {
+            // Arrange
+            _entryRepositoryMock
+                .Setup(r => r.Exists(oldFileName))
+                .Returns(true);
+
+            _directoryRepositoryMock
+                .Setup(r => r.Exists(newFileName.GetParentFullName()))
+                .Returns(true);
+
+            _entryRepositoryMock
+                .Setup(r => r.Exists(newFileName))
+                .Returns(false);
+
+            // Act
+            void Act() => _client.Move(oldFileName, newFileName);
+
+            // Assert
+            Assert.Throws<OperationIsInvalid>(Act);
         }
 
         [Theory]
